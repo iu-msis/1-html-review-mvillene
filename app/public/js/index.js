0aa2ddb01e4adf2533e27070f21dcebc4ea0a60a
mvillene <mvillene@iu.edu>
2021-10-20 15:43:30 -0400

    Updated Functionality

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,9 +1,12 @@
 const Offer = {
     data() {
       return {
-        "person": undefined,
+        person: undefined,
+        Book: [],
+        bookForm: {}
+
         }
-        "books";[]
+        
     },
     computed: {
         prettyBirthday() {
@@ -26,27 +29,6 @@ const Offer = {
             })
             console.log("B");
 
-        }
-        ,fetchBookData(){
-            fetch('/api/books/')
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
-                this.books = responseJson;
-                console.log(this.books);
-                console.log(books);
-            })
-            .catch( (err) => {
-                console.log(this.books);
-                console.error(err);
-            })
-        }
-    },
-    created() {
-        this.fetchUserData();
-        this.fetchBookData();
-    } //end created
-} // end Offer config
-Vue.createApp(Offer).mount('#offerApp');
+        },
 
 console.log("Z");
